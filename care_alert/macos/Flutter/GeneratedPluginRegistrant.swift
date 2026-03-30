c904780e300160bb2695fe72995f9e51674a9d0c
Niek Geerligs <geerligsniek@gmail.com>
2026-03-30 13:15:45 +0200

    added some packages

--- care_alert/macos/Flutter/GeneratedPluginRegistrant.swift
+++ care_alert/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import location
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
