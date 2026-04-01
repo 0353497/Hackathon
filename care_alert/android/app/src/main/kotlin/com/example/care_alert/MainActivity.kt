693a3ae3e34a2f9c0123a03257172ee032e61ea8
Andre <1355686@student.rocvantwente.nl>
2026-04-01 12:35:36 +0200

    Revert "AUthenication function added"

--- care_alert/android/app/src/main/kotlin/com/example/care_alert/MainActivity.kt
+++ care_alert/android/app/src/main/kotlin/com/example/care_alert/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.care_alert
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity : FlutterActivity()
